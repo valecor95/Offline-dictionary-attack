7c0c1d8814dfc8ea6b8d3a3e6e58678c34f4b1f1
Valerio Coretti <valeriocoretti@MBP-di-Valerio.homenet.telecomitalia.it>
2019-11-17 14:35:49 +0100

    Last

--- offline_dictionary_attack-hw2-1635747.c
+++ offline_dictionary_attack-hw2-1635747.c
@@ -4,120 +4,132 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/mman.h>
-#include <assert.h>
 #include <time.h>
-/*
+#include <ctype.h>
 #include <openssl/conf.h>
 #include <openssl/evp.h>
 #include <openssl/err.h>
 #include <openssl/opensslv.h>
 #include <openssl/crypto.h>
-#include <openssl/rand.h>
-#include <openssl/aes.h>
-#include <openssl/modes.h>
 
-void handleErrors(void)
-{
-    ERR_print_errors_fp(stderr);
-    abort();
+//INPUT COMMAND: openssl enc -aes-192-cbc -pbkdf2 -e -in <infile.txt> -out ciphertext.enc
+
+int plain_isascii(unsigned char *plaintext, int plaintext_len){
+	int i;
+	for(i=0; i < plaintext_len; i++){
+		int ch = plaintext[i];
+		if (isascii(ch)) continue;
+		else return 0;
+	}
+	return 1;
 }
 
-void decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
-            unsigned char *iv, unsigned char *plaintext)
-{
+int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *plaintext, const char *password, unsigned char* salt){
     EVP_CIPHER_CTX *ctx;
+    int len, plaintext_len, i;
 
-    int len;
+    //Simulation of the -pbkdf2 command with default parameters: iter = 10000, dgst = sha256
+    unsigned char tmpkeyiv[EVP_MAX_KEY_LENGTH + EVP_MAX_IV_LENGTH];
+    int iklen = EVP_CIPHER_key_length(EVP_aes_192_cbc());
+    int ivlen = EVP_CIPHER_iv_length(EVP_aes_192_cbc());
+    unsigned char key[iklen], iv[ivlen];
 
-    int plaintext_len;
-*/
-    /* Create and initialise the context */
-    //if(!(ctx = EVP_CIPHER_CTX_new()))
-    //    handleErrors();
+    PKCS5_PBKDF2_HMAC(password, strlen(password), salt, strlen((char*) salt), 10000, EVP_sha256(), iklen+ivlen, tmpkeyiv);
 
-    /*
-     * Initialise the decryption operation. IMPORTANT - ensure you use a key
-     * and IV size appropriate for your cipher
-     */
-		//if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_192_cbc(), NULL, key, iv))
-			//	handleErrors();
-    /*
-     * Provide the message to be decrypted, and obtain the plaintext output.
-     * EVP_DecryptUpdate can be called multiple times if necessary.
-     */
-    //if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
-    //    handleErrors();
-    //plaintext_len = len;
+		memcpy(key, tmpkeyiv, iklen);
+    memcpy(iv, tmpkeyiv+iklen, ivlen);
 
-    /*
-     * Finalise the decryption. Further plaintext bytes may be written at
-     * this stage.
-     */
-    //if(1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len))
-    //    handleErrors();
-    //plaintext_len += len;
+    //printf("Key: "); for(i=0; i < 24; ++i){ printf("%02x", key[i]); } printf("\n");
+    //printf("IV: "); for(i=0; i < 16; ++i){ printf("%02x", iv[i]); } printf("\n\n");
 
-    /* Clean up */
-    //EVP_CIPHER_CTX_free(ctx);
-//}
+    //Create and initialise the context
+    if(!(ctx = EVP_CIPHER_CTX_new()))
+        return -1;
+
+    //Initialise the decryption operation. IMPORTANT - ensure you use a key and IV size appropriate for your cipher
+		if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_192_cbc(), NULL, key, iv)) return -1;
+
+    //Provide the message to be decrypted, and obtain the plaintext output. EVP_DecryptUpdate can be called multiple times if necessary.
+    if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len)) return -1;
+    plaintext_len = len;
+
+    //Finalise the decryption. Further plaintext bytes may be written at this stage.
+    if(1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len)) return -1;
+    plaintext_len += len;
+
+    //Clean up
+    EVP_CIPHER_CTX_free(ctx);
+
+    return 1;
+}
 
 
-int main (int argc, char **argv)
-{
+int main (int argc, char **argv){
 
-	/* Message to be encrypted */
-	/*****************************************************************************************************************************/
-	unsigned char* dict;													// Structure for input file
-	unsigned long dict_size;
-  unsigned char* cipher;													// Structure for input file
-	unsigned long cipher_size;
+	unsigned char* dict;
+	unsigned char* cipher;
+	unsigned long dict_size, cipher_size;
+	clock_t start, end;													// clock for timing
+	int i = 0;
 
-  printf("*** READING DICTIONARY ***\n");
-  char* dictionary = argv[1];
-  char line[200];
-  char* res;
-	FILE* dict_fd = fopen(dictionary, "r");
-	if(dict_fd == NULL) fprintf(stderr, "Error in open file\n");
-	//dict_size = lseek(dict_fd, 0, SEEK_END);
-	//dict = malloc(sizeof(char)*dict_size);
-	//dict = (char*) mmap(0, dict_size, PROT_READ|PROT_WRITE, MAP_PRIVATE, dict_fdr, 0);
-  while(1) {
-    res = fgets(line, 200, dict_fd);
-    if(res == NULL)
-      break;
-    printf("%s", line);
-  }
-  fclose(dict_fd);
+	if(argc < 3){
+		printf("Usage ./offline_dictionary_attack <dictionary> <ciphertext>\n");
+		exit(1);
+	}
 
-  printf("*** READING CIPHERTEXT ***\n");
-  char* ciphertext = argv[2];
+	printf("*** READING CIPHERTEXT ***\n");
+	char* ciphertext = argv[2];
 	int cipher_fd = open(ciphertext, O_RDONLY, (mode_t)0666);
 	int cipher_fdr = cipher_fd;
 	if(cipher_fd == -1) fprintf(stderr, "Error in open file\n");
 	cipher_size = lseek(cipher_fd, 0, SEEK_END);
 	cipher = malloc(sizeof(char)*cipher_size);
 	cipher = mmap(0, cipher_size, PROT_READ|PROT_WRITE, MAP_PRIVATE, cipher_fdr, 0);
-	close(cipher_fd);
 
-	printf("*** END READING FILES ***\n\n");
-	/*****************************************************************************************************************************/
-	unsigned char* key_192 = malloc(sizeof(char)*24);
-	unsigned char* iv_192 = malloc(sizeof(char)*24);
-	unsigned char* aux_iv_192 = malloc(sizeof(char)*24);
-	clock_t start, end;																				                    // clock for timing
-	int i = 0;
-
-
-	printf("********************************************* Cipher Algorithm: AES *********************************************\n\n");
-	int plaintext_size = cipher_size;
+	unsigned long plaintext_size = cipher_size;
 	unsigned char* plaintext = malloc((sizeof(char)*plaintext_size));
 
-	/*printf("	DECRYPTING");
-	memcpy(aux_iv_192, iv_192, sizeof(iv_192));
-	start = clock();
-	decrypt(cipher, plaintext_size, key_128, aux_iv_128, plaintext);
-	end = clock();
+	printf("*** EXTRACTING SALT ***\n");
+	unsigned char salt[8];
+	for(i = 8; i < 16; i++){
+		salt[i-8] = cipher[i];
+	}
+	cipher = cipher + 16;																													//We don't want decrypt the salt
+	cipher_size -= 16;
 
-  printf("%s", plaintext);*/
-  return 0;
+	printf("SALT: "); for(i=0; i < 8; ++i){ printf("%02x", salt[i]); } printf("\n");
+	printf("CIPHER SIZE: %ld\n", cipher_size);
+
+	printf("*** DICTIONARY ATTACK ***\n");
+	char* dictionary = argv[1];
+	char* line = NULL;
+	ssize_t read;
+	size_t len = 0;
+	FILE* dict_fp = fopen(dictionary, "r");
+	if(dict_fp == NULL) fprintf(stderr, "Error in open file\n");
+
+	start = clock();
+	while(1){
+		read = getline(&line, &len, dict_fp);
+		unsigned char* password = malloc(sizeof(char)*(strlen(line)-1));
+		memcpy(password, line, (strlen(line)-1));
+		if(read == -1)
+			break;
+		if(decrypt(cipher, cipher_size, plaintext, (const char*)password, salt) == 1 && plain_isascii(plaintext, plaintext_size)){
+			printf("*** YOU WIN ***\n");
+			printf("PASSWORD: %s\n", password);
+			printf("%s\n", plaintext);
+			break;
+		}
+	}
+	end = clock();
+	fclose(dict_fp);
+
+	double dec_time = ((double) (end - start)) / CLOCKS_PER_SEC;
+	printf("  Time ===> %lf\n", dec_time);
+
+	free(plaintext);
+	free(cipher);
+
+	return 0;
 }
